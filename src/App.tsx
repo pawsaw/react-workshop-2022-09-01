e0a6192c3698d814ce971c3b5c523ad325ec6aeb
Pawel Sawicki <public@psawicki.de>
2022-09-02 08:51:47 +0200

    SimpleName component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { SimpleName } from './components/SimpleName';
+
+let count = 0;
+
+const Counter = () => {
+  const increment = () => {
+    count = count + 1;
+    console.log({ count });
+  };
+
+  return (
+    <button onClick={increment}>
+      {count}
+    </button>
+  );
+};
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Book Manager</h1>
+      <Counter />
     </div>
   );
 }
---
+++ src/components/SimpleName/SimpleName.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const name = 'Pawel';
+
+export const SimpleName: React.FC = () => {
+    return <p>Hello {name}</p>
+};
